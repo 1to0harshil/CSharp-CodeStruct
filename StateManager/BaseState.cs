e7448ab80727422f1d2db84cb3bc6f655e655754
Harshil jani <51168470+1to0harshil@users.noreply.github.com>
2023-10-30 12:33:04 +0530

    Add files via upload
    Code for StateManager

---
+++ StateManager/BaseState.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public abstract class BaseState<Estate> : MonoBehaviour where Estate : Enum {
+    public BaseState(Estate estate) {
+        Statekey = estate;
+    }
+    public Estate Statekey {get; private set;}
+    public abstract void EnterState();
+    public abstract void ExitState();
+    public abstract void UpdateState();
+    public abstract Estate GetNextState();
+    public abstract void OntriggerEnter(Collider other);
+    public abstract void OntriggerStay(Collider other);
+    public abstract void OntriggerExit(Collider other);
+    
+}
